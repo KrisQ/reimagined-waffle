2e70a83188e36d3b879901b6029b0a60e9c023e1
Kristenn Quemener <kquemener1@gmail.com>
2025-10-08 14:35:04 -0500

    qf

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 import pathlib
 from textnode import TextNode, TextType
@@ -10,7 +11,7 @@ def clean_up_dest(dest):
     os.mkdir(dest)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     dir_content = os.listdir(dir_path_content)
     for item in dir_content:
         print(f"source: {dir_path_content}")
@@ -25,6 +26,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 os.path.join(dir_path_content, item),
                 template_path,
                 os.path.join(dest_dir_path, item),
+                basepath,
             )
         # if not file_path.suffix == ".md":
         # raise Exception("Non..")
@@ -32,7 +34,10 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             html_filename = file_path.with_suffix(".html").name
             dest_file_path = os.path.join(dest_dir_path, html_filename)
             generate_page(
-                os.path.join(dir_path_content, item), template_path, dest_file_path
+                os.path.join(dir_path_content, item),
+                template_path,
+                dest_file_path,
+                basepath,
             )
 
 
@@ -69,7 +74,7 @@ def extract_title(markdown):
     raise Exception("no title found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_file = open(from_path, "r")
     markdown = markdown_file.read()
@@ -79,7 +84,12 @@ def generate_page(from_path, template_path, dest_path):
     template_file.close()
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    static_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    static_page = (
+        template.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/"', f'href="{basepath}"')
+        .replace('src="/"', f'src="{basepath}"')
+    )
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_dir):
         os.makedirs(dest_dir)  # Create parent directory only
@@ -89,8 +99,11 @@ def generate_page(from_path, template_path, dest_path):
 
 
 def main():
-    init_static("./static", "./public")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    init_static("./static", "./docs")
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 
 if __name__ == "__main__":
